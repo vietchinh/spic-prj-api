f7ebe4575910ccf63d528f6d7b1b8754e59f354d
BC46 <cs.melis@student.avans.nl>
2021-11-29 19:30:49 +0100

    Update Camera

--- Camera.hpp
+++ Camera.hpp
@@ -11,10 +11,21 @@ namespace spic {
      * @spicapi
      */
     class Camera : public GameObject {
-        private:
-            Color backgroundColor;
-            double aspectWidth;
-            double aspectHeight;
+    public:
+        explicit Camera(Color backgroundColor, const std::vector<std::shared_ptr<Component>>& components = {}, const std::string& parentName = "", const std::string& name = "", const std::string& tag = "", bool active = true, int layer = 0, double aspectWidth = 0, double aspectHeight = 0, bool autoInsert = false);
+        
+        const Color& BackgroundColor() const;
+        void BackgroundColor(const Color& newBackgroundColor);
+
+        double AspectWidth() const;
+        void AspectWidth(double newAspectWidth);
+
+        double AspectHeight() const;
+        void AspectHeight(double newAspectHeight);
+    private:
+        Color backgroundColor;
+        double aspectWidth;
+        double aspectHeight;
     };
 
 }
